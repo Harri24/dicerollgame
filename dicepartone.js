72611650870c625bfb8480cb1ddfa3c636ad5ac4
Harri New <harriet.new@tesglobal.com>
2019-05-01 16:53:23 +0100

    finished part 4 of dicetask

--- dicepartone.js
+++ dicepartone.js
@@ -2,11 +2,30 @@ const readlineSync = require('readline-sync');
 
 console.log("Welcome to the Dice! ");
 
-let diceNumber = readlineSync.question('How many dice would you like to roll? ');
-
+function numOfDice(diceNumber, diceFace){
     let rolls = [];
     for (i = 0; i < diceNumber; i++) {
-        rolls.push(Math.ceil( Math.random() * 6 ));
+        rolls.push(Math.ceil( Math.random() * diceFace ));
     }
 
-console.log(`The results of your dice are ${rolls} `);
+    const sum = rolls.reduce((total, amount) => total + amount); 
+
+console.log(`The results of your dice are ${sum} `);
+}
+
+
+function getNumberInput(userPrompt){
+    while (true){
+   let userResponse = parseInt(readlineSync.question(userPrompt));
+   if (!isNaN(userResponse) && userResponse > 0){
+       return userResponse
+   }
+   console.log("Invalid response");
+    }
+}
+
+let diceSize = getNumberInput("What size dice would you like to use");
+
+let diceNumber = getNumberInput('How many dice would you like to roll? ');
+numOfDice(diceNumber, diceSize); 
+
